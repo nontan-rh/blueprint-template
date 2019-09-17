cbb5b960377b849ac37c43c1aac0d0ddbb0c273e
nontan <nontan@pixiv.com>
2019-09-17 21:57:43 +0900

    Setup eslint

--- components/Root.tsx
+++ components/Root.tsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import { Button } from "@blueprintjs/core";
 
+interface Props {
+}
 
-export default class Root extends Component {
+interface State {
+    counter: number,
+}
+
+export default class Root extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    onClick = () => {
+        const { counter } = this.state;
+        this.setState({ counter: counter + 1 });
+    }
+
     render() {
-        return (<Button intent="success" text="Hello world" />);
+        const { counter } = this.state;
+        return (<Button intent="success" text={`Hello world ${counter}`} onClick={this.onClick} />);
     }
 }
