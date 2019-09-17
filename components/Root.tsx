9fc573472a6c8745e423734e60707157b342fc57
nontan <nontan@pixiv.com>
2019-09-17 21:29:35 +0900

    Init with parcel

---
+++ components/Root.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Button } from "@blueprintjs/core";
+
+
+export default class Root extends Component {
+    render() {
+        return (<Button intent="success" text="Hello world" />);
+    }
+}
